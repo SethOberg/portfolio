a712379886420c684d73269fa923fdfad86b5ce5
SethOberg <seth.a.oberg@gmail.com>
2023-04-12 14:48:19 +0200

    Feat: added navbar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route } from "react-router";
+import About from "./pages/About";
+import Main from "./pages/Main";
+import Projects from "./pages/Projects";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/Projects" element={<Projects />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/components/Navbar.js
@@ -0,0 +1,52 @@
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import CircleIcon from "@mui/icons-material/Circle";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import { BorderBottom } from "@mui/icons-material";
+
+function printSomething() {
+  console.log("Button clicked!!!");
+}
+
+const Navbar = () => {
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar
+        position="static"
+        sx={{
+          background: "white",
+          color: "#333",
+          borderBottom: 1,
+          borderColor: "#A9A9A9",
+          boxShadow: 0,
+        }}
+      >
+        <Toolbar>
+          <CircleIcon sx={{ mr: 2, color: "#E17A00" }} fontSize="large" />
+          <Typography variant="h8" component="div" sx={{ flexGrow: 1 }}>
+            PORTFOLIO
+          </Typography>
+
+          <IconButton
+            onClick={() => printSomething()}
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+};
+
+export default Navbar;

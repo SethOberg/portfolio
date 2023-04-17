f229668bb89086a0cc8544cf07a599e4c716bf86
SethOberg <seth.a.oberg@gmail.com>
2023-04-17 11:58:08 +0200

    Feat: added text to portfolio

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,14 +1,21 @@
 import React from "react";
 import "../styles/mainPage.css";
+import SocialLinks from "../components/SocialLinks";
 
 const Main = () => {
   return (
-    <div id="indexContent">
-      Welcome to my portfolio!
-      <br />
-      <br />
-      This website was developed with React and Material UI.
-    </div>
+    <>
+      <div id="indexContent">
+        <p className="textMediumTitle">Hello!</p>
+        Welcome to my portfolio!
+        <br />
+        <br />
+        This website was developed with React and Material UI.
+      </div>
+      <div id="indexContent">
+        <SocialLinks showLinks={true} />
+      </div>
+    </>
   );
 };
 
